560e1402bb758136dcaaa9056050b2b344cf1f50
Qccccc's laptop <896853205@qq.com>
2019-08-21 02:04:36 +0800

    初始化事务管理

--- src/resources/db-connect.js
+++ src/resources/db-connect.js
@@ -33,5 +33,51 @@ export const db = {
         }
       })
     });
+  },
+
+  /**
+   * 事务处理
+   * @param {Array} sqlArr [{sqlLan: 'sql语句', sqlParams: ['sql参数1', 'sql参数2']}]
+   */
+  transactions (sqlArr) {
+    try {
+      let connection = mysql.createConnection(dbConfig);
+
+      connection.beginTransaction(err => {
+
+        if (err) { 
+          throw err 
+        }
+
+        sqlArr.forEach(sql => {
+          db.transaction(connection, sql.sqlLan, sql.sqlParams);
+        })
+      })
+    } catch(err) {
+      console.error('事件处理发生错误:', err);
+    }
+  },
+  /**
+   * 事务处理的一次处理
+   * @param {Object} connection sql链接
+   * @param {String} sqlLan sql语句
+   * @param {Array} sqlParams sql语句的参数
+   */
+  transaction (connection, sqlLan, sqlParams) {
+    connection.query(sqlLan, sqlParams, err => {
+      if (err) {
+        return connection.rollback(() => {
+          throw err
+        })
+      } else {
+        connection.commit(err => {
+          if (err) {
+            return connection.rollback(() => {
+              throw err
+            })
+          }
+        })
+      }
+    })
   }
 };
